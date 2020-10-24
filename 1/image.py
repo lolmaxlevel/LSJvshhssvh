0980e2593b63f811653f64d5214ada04d2c1c2b5
kyaleee <63292579+kyaleee@users.noreply.github.com>
2020-10-24 20:25:08 +0300

    kto

--- 1/image.py
+++ 1/image.py
@@ -1,32 +1,33 @@
 from PIL import Image, ImageDraw, ImageFont
 
-im = Image.open('C:/Users/sheli/PycharmProjects/CoronaBot/chel.jpg')
+im = Image.open('photo/krut.jpg')
 
 
-def gen_text(data):
+def gen_text(data_infected, data_dead, data_recovered):
     draw_text = ImageDraw.Draw(im)
-    font = ImageFont.truetype("arial.ttf", 60)
+    font = ImageFont.truetype("arial.ttf", 30)
     draw_text.text(
-        (100, 20),
-        data,
-        font = font,
-        fill=('#1C0606')
-        )
-    draw_text.text(
-        (100, 434),
-        data,
-        font = font,
+        (315, 30),
+        data_infected,
+        font=font,
         fill=('#1C0606')
     )
     draw_text.text(
-        (100, 217),
-        data,
+        (140, 120),
+        data_dead,
+        font=font,
+        fill=('#1C0606')
+    )
+    draw_text.text(
+        (520, 120),
+        data_recovered,
         font=font,
         fill=('#1C0606')
     )
     im.show()
 
-data = 'test text'
 
-
-gen_text(data)
+data_dead = '1000'
+data_recovered = '14561'
+data_infected = '136241'
+gen_text(data_infected, data_dead, data_recovered)
